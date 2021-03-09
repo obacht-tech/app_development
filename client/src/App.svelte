932e1fa397383a94e1d4c81a8754a64ff2fbb742
v-rogg <itsabattleship@gmail.com>
2021-03-09 13:05:57 +0100

    Add svelte

---
+++ client/src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    console.log("Hello from Svelte!")
+</script>
+
+<style global lang="sass">
+    @import "global"
+
+    main
+        padding: 1em
+        margin: 0
+        font-family: "Lexend Deca", Arial, sans-serif
+        display: grid
+        grid-template-columns: 1fr minmax(0, 2fr)
+        grid-template-rows: 22rem auto auto
+        height: calc(100vh - 4rem)
+
+    footer
+        margin: 1rem 0 0 1rem
+        font-size: $size-small
+
+    #creator
+        text-decoration: none
+        color: $x-cyan
+</style>
+
+<main>
+    <footer>
+        <strong>Hello</strong> brainy friend! 😃 Got Feedback? Contact <a href="mailto:valentin.rogg@xentral.com" id="creator">@ValentinRogg</a>
+    </footer>
+</main>
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use std::path::{Path, PathBuf};
+use rocket::response::{NamedFile, Redirect};
+use rocket_contrib::templates::Template;
+
+#[get("/")]
+fn index() -> Template {
+    let map = ();
+    Template::render("index", &map)
+}
+
+#[get("/client/<file..>")]
+fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("client/public/build").join(file)).ok()
+}
+
+#[catch(404)]
+fn not_found() -> Redirect {
+    Redirect::to("/")
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .mount("/", routes![index, files])
+        .attach(Template::fairing())
+        .register(catchers![not_found])
+        .launch();
 }
