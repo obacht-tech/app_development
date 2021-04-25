b5fec07aeee50f3be0d364cce0c643ec115c4953
v-rogg <itsabattleship@gmail.com>
2021-04-25 21:27:43 +0200

    Update App

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-    // import Three from "./fcomponents/Three/Three.svelte"
-    // import Timeline from "./components/Timeline.svelte"
-    // import Controls from "./components/Controls.svelte"
-    import TextCard from "./components/TextCard.svelte";
-    import {cameraLocked} from "./store";
+    import TextCard from "./components/Story/TextCard.svelte";
     import Header from "./components/Story/Header.svelte";
     import Footer from "./components/Story/Footer.svelte";
-    import Application from "./components/Application.svelte";
+    import ThreePerson from "./components/Three/ThreePerson.svelte";
+    import ThreeHeatmap from "./components/Three/ThreeHeatmap.svelte";
+    import ThreeFull from "./components/Three/Three.svelte";
 </script>
 
 <style global lang="sass">
@@ -27,12 +25,11 @@
 </svelte:head>
 
 <body class="scroll-snap-container">
-    <div class="scroll-snap-child"></div>
-    <Header scrollSnapChild />
+    <Header scrollSnapChild/>
 
     <main>
 
-        <Application aid="person" scrollSnapChild />
+        <ThreeFull aid="person" scrollSnapChild/>
 
         <TextCard>
             Vor einem Jahr ließ sich unser Leben in New York am besten als apokalyptisch beschreiben. Durch die
@@ -42,28 +39,26 @@
             neuen Arbeitslosen im Viertel herankarrte.
         </TextCard>
 
-        <Application aid="heatmap" scrollSnapChild />
+        <ThreeFull aid="heatmap" scrollSnapChild/>
 
-<!--        <section class="application scroll-snap-child">-->
-<!--            <Three canvasName="app0" controlsEnabled={false}/>-->
-<!--            <Timeline/>-->
-<!--            <Controls layers={false} camera={false}/>-->
-<!--        </section>-->
+        <TextCard>
+            Vor einem Jahr ließ sich unser Leben in New York am besten als apokalyptisch beschreiben. Durch die
+            leeren Straßenzüge hallten die Sirenen der Ambulanzen im Fünfminutentakt. Vor dem Beerdigungsinstitut,
+            zwei Blocks von unserem Haus entfernt, parkte ein Kühlwagen, weil die Leichenhalle dort überfüllt war.
+            Die pakistanische Gemeinde organisierte den Hunger Truck, einen Minivan, der Lebensmittel für die vielen
+            neuen Arbeitslosen im Viertel herankarrte.
+        </TextCard>
 
+        <TextCard>
+            Vor einem Jahr ließ sich unser Leben in New York am besten als apokalyptisch beschreiben. Durch die
+            leeren Straßenzüge hallten die Sirenen der Ambulanzen im Fünfminutentakt. Vor dem Beerdigungsinstitut,
+            zwei Blocks von unserem Haus entfernt, parkte ein Kühlwagen, weil die Leichenhalle dort überfüllt war.
+            Die pakistanische Gemeinde organisierte den Hunger Truck, einen Minivan, der Lebensmittel für die vielen
+            neuen Arbeitslosen im Viertel herankarrte.
+        </TextCard>
 
-<!--        <section class="application scroll-snap-child">-->
-<!--            <Three canvasName="app1" controlsEnabled={false}/>-->
-<!--            <Timeline/>-->
-<!--            <Controls layers={false} camera={false}/>-->
-<!--        </section>-->
-
-
-<!--        <section class="application scroll-snap-child">-->
-<!--            <Three canvasName="app2" controlsEnabled={!$cameraLocked}/>-->
-<!--            <Timeline/>-->
-<!--         <Controls layers={true} camera={true}  on:cameraLock={cameraLocked.set(!$cameraLocked)}/>-->
-<!--        </section>-->
+        <ThreeFull scrollSnapChild/>
     </main>
 
-    <Footer scrollSnapChild />
+    <Footer scrollSnapChild/>
 </body>
