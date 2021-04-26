120e2be6caec16f9ff9d9d5db4ab0dae816983c6
silvia tosato <sisa.t.99@gmail.com>
2021-04-26 22:07:05 +0200

    added timeline structure with inputs

--- client/src/components/Three/Timeline/Timeline.svelte
+++ client/src/components/Three/Timeline/Timeline.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
+    import RangeSlider from "svelte-range-slider-pips";
+    import {spring} from "svelte/motion";
 
+    export let springValues = {stiffness: 0.15, damping: 0.4};
+    export let indicator: boolean = true;
+    export let indicatorValue: number = 0
     export let datasetStart: Date;
     export let datasetEnd: Date;
     export let markerStart: Date;
     export let markerNow: Date;
     export let markerEnd: Date;
+
+
+    let indicatorSpring = spring({value: 0}, {
+        stiffness: 0.4, damping: 0.7
+    });
 
 </script>
 
@@ -22,30 +32,27 @@
         background: white
         box-shadow: $shadow-2xl
         padding: 1.5rem 1.5rem 1rem 1.5rem
+        .rangeSlider
+            margin: 0
+            padding: 0
 
-        &__bar
-            display: block
-            background: $dark-grey
-            width: 100%
-            height: 1rem
-            border-radius: $border-radius
+        &__container
+            position: relative
+
+            .indicator
+                margin: 0
+                position: absolute
+                bottom: 1rem
+                width: 100%
+                z-index: 5
+
 
         &__legend
             display: flex
             justify-content: space-between
             margin-top: 1rem
 
-            >div
-                position: relative
 
-                &:before
-                    display: block
-                    position: absolute
-                    content: ''
-                    width: 3px
-                    background: $dark-grey
-                    height: 1rem
-                    bottom: 1.75rem
 
             &--start
                 &:before
@@ -55,40 +62,34 @@
                 &:before
                     right: 0
 
-        &__marker
-            position: absolute
-            bottom: 1rem
-            left: 37%
-            text-align: center
-            color: $blue
 
-            &:before
-                display: block
-                position: absolute
-                content: ''
-                width: 3px
-                background: $blue
-                height: 2.5rem
-                left: 50%
-                bottom: 1.75rem
-
-            &--icon
-                position: absolute
-                left: calc(50% - 0.7rem)
-                bottom: 3rem
-                font-size: 2.5rem
 
 </style>
 
 <div class="timeline container">
-    <div class="timeline__bar">
+
+    <div class="timeline__container">
+        {#if indicator}
+            <div class="indicator__container">
+                <input on:change={(value) => console.log($indicatorSpring.value)} bind:value={$indicatorSpring.value}
+                       class="indicator" type=range min=0 max={100}/>
+            </div>
+        {/if}
+        <RangeSlider on:stop={(value) => console.log(value)} class="rangeSlider" hover={false} range values={[30,70]}/>
     </div>
     <div class="timeline__legend">
-        <div class="timeline__legend--start">{datasetStart.getHours() + ":" + datasetStart.getMinutes()} Uhr</div>
-        <div class="timeline__legend--end">{datasetEnd.getHours() + ":" + datasetEnd.getMinutes()} Uhr</div>
+        <div class="timeline__legend&#45;&#45;start">{datasetStart.getHours() + ":" + datasetStart.getMinutes()} Uhr</div>
+        <div class="timeline__legend&#45;&#45;end">{datasetEnd.getHours() + ":" + datasetEnd.getMinutes()} Uhr</div>
     </div>
-    <div class="timeline__marker">
-        14:27 Uhr
-        <i class="fas fa-caret-down  timeline__marker--icon"></i>
-    </div>
+
+    <!--    <div class="timeline__bar">
+        </div>
+        <div class="timeline__legend">
+            <div class="timeline__legend&#45;&#45;start">{datasetStart.getHours() + ":" + datasetStart.getMinutes()} Uhr</div>
+            <div class="timeline__legend&#45;&#45;end">{datasetEnd.getHours() + ":" + datasetEnd.getMinutes()} Uhr</div>
+        </div>
+        <div class="timeline__marker">
+            14:27 Uhr
+            <i class="fas fa-caret-down  timeline__marker&#45;&#45;icon"></i>
+        </div>-->
 </div>
