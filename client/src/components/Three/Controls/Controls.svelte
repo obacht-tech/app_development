5047c044bc88f00d344396240e0681450991ff3b
silvia tosato <sisa.t.99@gmail.com>
2021-05-03 09:49:33 +0200

    added controls to timeline

--- client/src/components/Three/Controls/Controls.svelte
+++ client/src/components/Three/Controls/Controls.svelte
@@ -1,24 +1,17 @@
 <script lang="ts">
     import Zoom from "./Zoom.svelte";
-    import Playback from "./Playback.svelte";
     import Layers from "./Layers.svelte";
-    import {modal} from "../../../store";
-    import Popup from './Popup.svelte';
+    import Settings from "./Settings.svelte";
 
     type PlaybackState = "play" | "2x forward" | "stop";
     type LayerState = "person" | "heatmap" | "paths" | "full";
 
     export let zoomLock: boolean = false;
-    export let playback: boolean = false;
     export let layers: boolean = false;
+    export let settings: boolean = false;
     export let cameraZoomLocked: boolean = true;
-    export let playbackState: PlaybackState;
     export let layerState: LayerState;
 
-    const showPopup = () => {
-        modal.set(Popup);
-        console.log('CKLCK')
-    };
 
 </script>
 
@@ -27,15 +20,16 @@
 
     .controls
         position: absolute
-        bottom: 3rem
+        bottom: 2.5rem
         width: clamp(0px, 100vw, 400px)
         left: 0
         right: 0
         margin: 0 auto
-        padding: 0 2rem
+        padding: .5rem
         font-size: 1.5rem
-        display: grid
-        grid-template-columns: 1fr 1fr 1fr
+        display: flex
+        justify-content: center
+        //grid-template-columns: 1fr 1fr 1fr
 </style>
 
 <div class="controls container">
@@ -43,12 +37,11 @@
         <Zoom bind:cameraZoomLocked={cameraZoomLocked}/>
     {/if}
 
-    {#if playback}
-        <Playback bind:playbackState={playbackState}/>
-    {/if}
     {#if layers}
-        <div on:click={showPopup}>
             <Layers bind:layerState={layerState}/>
-        </div>
+    {/if}
+
+    {#if settings}
+        <Settings bind:layerState={layerState}/>
     {/if}
 </div>
