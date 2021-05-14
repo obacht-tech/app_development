fb4678535feb23d83ec4d2f9618897dc19a34fe0
silvia tosato <sisa.t.99@gmail.com>
2021-05-14 12:03:43 +0200

    splines positioning timebased added

--- client/src/components/Three/Canvas.svelte
+++ client/src/components/Three/Canvas.svelte
@@ -24,7 +24,7 @@
     positions.subscribe((data: { data?: PositionData[] }) => {
         if (data.data) {
             fetchingData = data.data;
-           positionSplines = initSplines()
+            positionSplines = initSplines()
         }
     })
 
@@ -38,13 +38,13 @@
                 })
 
                 if (foundPersonIndex > -1) {
-                    peopleSplines[foundPersonIndex].splineData.push( new THREE.Vector2( person.pos[0],  person.pos[1]))
+                    peopleSplines[foundPersonIndex].splineData.push(new THREE.Vector2(person.pos[0], person.pos[1]))
 
-                }else{
+                } else {
 
                     const newPerson = {
                         pid: person.pid,
-                        splineData: [new THREE.Vector2( person.pos[0],  person.pos[1])],
+                        splineData: [new THREE.Vector2(person.pos[0], person.pos[1])],
                         startDate: fetchingData[i].date,
                         timePosition: i
 
@@ -55,9 +55,9 @@
             }
         }
 
-        for (let personSpline of peopleSplines){
+        for (let personSpline of peopleSplines) {
             personSpline.spline = new THREE.SplineCurve(personSpline.splineData);
-           personSpline.timeDelta = personSpline.splineData.length-1;
+            personSpline.timeDelta = personSpline.splineData.length - 1;
             generatePersonMesh(personSpline)
         }
 
@@ -66,7 +66,7 @@
 
     const people = new THREE.Group()
 
-    function generatePersonMesh(person: PersonSpline){
+    function generatePersonMesh(person: PersonSpline) {
         const personMesh = SkeletonUtils.clone(humanMesh);
         const personMaterial = new THREE.MeshStandardMaterial({
             color: '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
@@ -137,50 +137,6 @@
             });
         });
 
-        // const path = new THREE.Path();
-        //
-        // path.lineTo(0, 0.8);
-        // path.quadraticCurveTo(0, 1, 0.2, 1);
-        // path.lineTo(1, 1);
-        //
-        // const points = path.getPoints();
-        //
-        // const geometry = new THREE.BufferGeometry().setFromPoints(points);
-        // const material = new THREE.LineBasicMaterial({color: 0xffffff});
-        //
-        // const line = new THREE.Line(geometry, material);
-        // scene.add(line);
-
-
-
-        // Create a sine-like wave
-        const curve = new THREE.SplineCurve( [
-            new THREE.Vector2( -10, 0 ),
-            new THREE.Vector2( -5, 5 ),
-            new THREE.Vector2( 0, 0 ),
-            new THREE.Vector2( -10, 0 ),
-            new THREE.Vector2( -5, 5 ),
-            new THREE.Vector2( -10, 0 ),
-            new THREE.Vector2( -5, 5 ),
-        ] );
-
-        const curve2 = new THREE.SplineCurve( [
-            new THREE.Vector2( -10, 0 ),
-            new THREE.Vector2( -5, 5 ),
-
-        ] );
-
-       // const points = curve.getPoints( 50 );
-        console.log(curve.getLength())
-        //console.log(curve2.arcLengthDivisions)
-       // const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-        const material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-
-// Create the final object to add to the scene
-      //  const splineObject = new THREE.Line( geometry, material );
-
-
         const renderer = new THREE.WebGLRenderer({
             canvas: canvas,
         });
@@ -219,140 +175,41 @@
             renderOnce();
         })
         // People
-        let nextPositions: Person[] = [];
-
-
-
-
-
-        function personModels(num: number) {
-            if (fetchingData.length === 0 || !humanMesh || num >= fetchingData.length) {
-                return
-            }
-
-            //TODO: performence
-            const addedPersons: string[] = [];
-            nextPositions = fetchingData[num + 1].people;
-
-            for (let person of fetchingData[num].people) {
-                let findIndexPerson = people.children.findIndex((personValue) => {
-                    return personValue.uuid === person.pid
-                })
-                if (findIndexPerson > -1) {
-                    people.children[findIndexPerson].position.x = person.pos[0] * positionScaling;
-                    people.children[findIndexPerson].position.z = person.pos[1] * positionScaling;
-                } else {
-                    //random color
-                    const personMesh = SkeletonUtils.clone(humanMesh);
-                    const personMaterial = new THREE.MeshStandardMaterial({
-                        color: '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
-                    })
-                    personMesh.position.x = person.pos[0] * positionScaling;
-                    personMesh.position.z = person.pos[1] * positionScaling;
-                    personMesh.uuid = person.pid;
-                    personMesh.material = personMaterial;
-
-                    people.add(personMesh)
-                }
-                addedPersons.push(person.pid);
-                const nextPositionIndex = nextPositions.findIndex((el) => {
-                    return person.pid === el.pid;
-                })
-                if (nextPositionIndex > -1) {
-                    const deltaX = (nextPositions[nextPositionIndex].pos[0] - person.pos[0])
-                    const deltaZ = (nextPositions[nextPositionIndex].pos[1] - person.pos[1])
-                    nextPositions[nextPositionIndex].deltaNext = [deltaX, deltaZ]
-                }
-            }
-
-            for (let person of people.children) {
-                let findIndexPerson = addedPersons.findIndex((personId) => {
-                    return personId === person.uuid
-                })
-                if (findIndexPerson === -1) {
-                    people.remove(person)
-                }
-            }
-        }
-
-
         scene.add(people)
 
-
         function updatePositions(time, second) {
-            // scene people
-            /*for (let person of people.children) {
-
-                const nextPositionIndex = nextPositions.findIndex((el) => {
-                    return person.uuid === el.pid;
-                })
-                if (nextPositionIndex > -1) {
-                    const deltaPositionX = nextPositions[nextPositionIndex].pos[0] * positionScaling - person.position.x;
-                    const deltaPositionZ = nextPositions[nextPositionIndex].pos[1] * positionScaling - person.position.z;
-                    person.position.x += (nextPositions[nextPositionIndex].deltaNext[0] * positionScaling) / 60
-                    person.position.z += nextPositions[nextPositionIndex].deltaNext[1] * positionScaling / 60
-                }
-
-
-            }*/
-
-            for (let person of people.children){
+            for (let person of people.children) {
                 const moment = time - person.timePosition; // 1.2 sek
-                if(person.timePosition <= second && moment <=  person.timeDelta ){
-                    if(!person.visible){
+                if (person.timePosition <= second && moment <= person.timeDelta) {
+                    if (!person.visible) {
                         person.visible = true;
                     }
-
-
                     const deltaTimePosition = person.timeDelta; //diffrenz ende-starttime
-
-
-
-                    const pos =  person.spline.getPoint((moment*100/deltaTimePosition)*0.01);
-
-                   person.position.x =pos.x*positionScaling
-                    person.position.z = pos.y*positionScaling
-
-
-                }else{
-                    if(person.visible){
+                    const pos = person.spline.getPoint((moment * 100 / deltaTimePosition) * 0.01);
+                    person.position.x = pos.x * positionScaling
+                    person.position.z = pos.y * positionScaling
+                } else {
+                    if (person.visible) {
                         person.visible = false
                     }
                 }
-
-
-
             }
-
-
         }
 
         /**
          * Animate
          */
-        const clock = new THREE.Clock();
-
-
         let last = 0;
-        let num = 0;
+        let fullSeconds = 0;
         let speed = 1;
-        let elapsedTime = 0;
-        let t = 0
+
         function render(timeStamp?) {
             let timeInSecond = timeStamp / 1000;
-           // elapsedTime = clock.getElapsedTime()
             if (timeInSecond - last >= speed) {
                 last = timeInSecond;
-
-                // personModels(num)
-                ++num
-
+                ++fullSeconds
             }
-
-            //console.log(timeInSecond)
-
-            updatePositions(timeInSecond,num)
-t+=0.000016
+            updatePositions(timeInSecond, fullSeconds)
             window.requestAnimationFrame(render);
             if (inFrame) {
                 controls.enableZoom = !cameraZoomLocked;
