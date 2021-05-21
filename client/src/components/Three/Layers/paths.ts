a762fa433b57d1a94662f97a8fc3ddaf14cdd39c
silvia tosato <sisa.t.99@gmail.com>
2021-05-21 13:06:40 +0200

    adjusted paths; increased divsions

--- client/src/components/Three/Layers/paths.ts
+++ client/src/components/Three/Layers/paths.ts
@@ -4,7 +4,7 @@ import type {PersonSpline} from "../../../types";
 export function generatePaths(people: PersonSpline[]): THREE.Group {
     const paths = new THREE.Group()
     for (let personSpline of people) {
-        const points = personSpline.spline.getPoints(50);
+        const points = personSpline.spline.getPoints(200);
         const geometry = new THREE.BufferGeometry().setFromPoints(points);
         const color = new THREE.Color(0xffffff);
         color.setHex(Math.random() * 0xffffff);
@@ -25,6 +25,7 @@ export function generatePaths(people: PersonSpline[]): THREE.Group {
 
 export function rangePaths(paths: THREE.Group, start: number, end: number) {
     for (let path of paths.children) {
-        path.visible = path.timePosition >= start && path.timePosition + path.timeDelta <= end;
+        const pathRange = path.timePosition + path.timeDelta;
+        path.visible = (path.timePosition >= start && path.timePosition <= end) || (pathRange >= start && pathRange <= end);
     }
 }
