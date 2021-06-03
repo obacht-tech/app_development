59dd55e22526bfeb5dced7a3e6c7f106a9f7f33f
silvia tosato <sisa.t.99@gmail.com>
2021-06-03 12:10:44 +0200

    added only range to timeline props

--- client/src/components/Three/Timeline/Timeline.svelte
+++ client/src/components/Three/Timeline/Timeline.svelte
@@ -7,6 +7,7 @@
 
     export let playbackState: PlaybackState;
     export let indicator: boolean = false;
+    export let range: boolean = true;
     export let datasetStart: Date;
     export let datasetEnd: Date;
     export let markerStart: Date;
@@ -15,8 +16,8 @@
 
     const diffSeconds = Math.floor(((datasetEnd - datasetStart) / 1000) );
     export let playback: boolean = false;
-    let indicatorValue: number = 0;
-    let markerStartValue: number = 0;
+    let indicatorValue: number = 1;
+    let markerStartValue: number = 1;
     let markerEndValue: number = diffSeconds;
     let userInteraction: boolean = false;
 
@@ -30,15 +31,17 @@
 
             render()
         }
+        if(range){
+            markerStartEndSeconds.subscribe((data: { startValue: number, endValue: number }) => {
+                if (data.startValue && data.endValue) {
+                    markerStart = addSeconds(datasetStart, data.startValue);
+                    markerEnd = addSeconds(datasetStart, data.endValue);
+                    markerStartValue = data.startValue;
+                    markerEndValue = data.endValue;
+                }
+            })
+        }
 
-        markerStartEndSeconds.subscribe((data: { startValue: number, endValue: number }) => {
-            if (data.startValue && data.endValue) {
-                markerStart = addSeconds(datasetStart, data.startValue);
-                markerEnd = addSeconds(datasetStart, data.endValue);
-                markerStartValue = data.startValue;
-                markerEndValue = data.endValue;
-            }
-        })
     })
 
     let last = 0;
@@ -66,7 +69,7 @@
 
     function formatDate(date: Date) {
         const minutes = date.getMinutes();
-        return date.getHours() + ':' + ((minutes < 10) ? 0 : '') + minutes +':'+ date.getSeconds() +'Uhr'
+        return date.getHours() + ':' + ((minutes < 10) ? 0 : '') + minutes +':'+ ((date.getSeconds() < 10) ? 0 : '')+date.getSeconds() +'Uhr'
     }
 
     function setMarkerStartEnd(startValue: number, endValue: number) {
@@ -139,10 +142,10 @@
 <div class="timeline container">
     <div class="timeline__container">
         {#if indicator}
-            <div class="indicator_slider__container">
+            <div class="indicator_slider__container"  class:only={!range}>
                 <RangeSlider
                         id="indicator_slider"
-                        min={0}
+                        min={1}
                         max={diffSeconds}
                         values={[indicatorValue]}
                         formatter={ value => formatDate(addSeconds(datasetStart, value)) }
@@ -153,18 +156,21 @@
 
             </div>
         {/if}
+        {#if range}
         <div class="range_slider__container">
             <RangeSlider
                     id="range_slider"
-                    min={0}
+                    min={1}
                     max={diffSeconds}
                     pushy={true}
                     hover={true}
                     range float
                     formatter={ value => formatDate(addSeconds(datasetStart, value)) }
                     on:stop={(value) =>setMarkerStartEnd(value.detail.values[0], value.detail.values[1])}
-                    values={[markerStartValue, markerEndValue]}/>
+                    values={[markerStartValue, markerEndValue]}
+                    springValues={ {stiffness: 1, damping: 1 }}/>
         </div>
+        {/if}
     </div>
     <div class="timeline__legend">
         <div class="timeline__legend&#45;&#45;start">{formatDate(datasetStart)}
